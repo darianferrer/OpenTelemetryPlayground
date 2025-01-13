51391982a274f979f26c324bfed11bc321b1f309
darianferrer <darian.ferrer@thetrainline.com>
2025-01-13 08:06:34 +0000

    Add tenant trace processor

---
+++ src/Customer.Api/Telemetry/TenantProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Customer.Api.Data;
+using OpenTelemetry;
+
+namespace Customer.Api.Telemetry;
+
+public class TenantProcessor : BaseProcessor<Activity>
+{
+    private readonly TenantProvider _tenantProcessor;
+
+    public TenantProcessor(TenantProvider tenantProcessor)
+    {
+        _tenantProcessor = tenantProcessor;
+    }
+
+    public override void OnEnd(Activity data)
+    {
+        data.SetTag(TagNames.TenantId, _tenantProcessor.TenantId());
+        base.OnEnd(data);
+    }
+}
